9663d9860b20553b04490d86baea8c52d7e56b2e
maxi4747828 <b3772346@gmail.com>
2025-04-29 18:37:27 -1200

    Add files via upload

---
+++ src/pages/Ref.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Ref = () => {
+  return <div>This Is Not Full Code, dm on telegram @ETBCEO For Full Code. We can clone Hamster Tapswap Any miniapp bot</div>;
+};
+
+export default Ref;
---
+++ src/pages/Stats.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Stats = () => {
+  return <div>This Is Not Full Code, dm on telegram @ETBCEO For Full Code. We can clone Hamster Tapswap Any miniapp bot</div>;
+};
+
+export default Stats;
